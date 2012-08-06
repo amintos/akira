9ec0c829863759aedf938f02cd36b68e31724dab
Nicco Kunzmann <niccokunzmann@googlemail.com>
2012-08-06 19:58:11 +0200

    send, async and sync communication is tested

--- process/test_reference.py
+++ process/test_reference.py
@@ -1,7 +1,18 @@
+
+import sys
+import time
+
 from Process import thisProcess
 import unittest
 
+from test_multi_Process import timeout, TIMEOUT
 from reference import *
+
+from multiprocessing.pool import Pool
+
+from LocalObjectDatabase import LocalObjectDatabase
+
+from StringIO import StringIO
 
 class MockReference(object):
 
@@ -38,10 +49,150 @@ class TestObject(object):
     def getA(self):
         return self.a
 
-class TestSend(unittest.TestCase):
-    
-    def setUp(self):
-        pass
+thisProcess.listenOnIPv4()
 
+def g():
+    pass
+
+class DB(LocalObjectDatabase):
+    pass
+
+db = DB()
+
+ref = db.store
+class X(object):
+
+    def __init__(self):
+        self.value = None
+        self._start = 'wait'
+
+    def setValue(self, newValue):
+        self.value = newValue
+        return 'valueSet'
+
+    def setValueWithError(self, value):
+        self.setValue(value)
+        raise ValueError('error!')
+
+    def setValue_wait_delete(self, value, after = None):
+        self.setValue(value)
+        self.wait()
+        self.setValue(after)
+        return (value, after)
+
+    def start(self):
+        self._start = 'start'
+        
+    def wait(self):
+        timeout(lambda: self._start, 'wait')
+        if self._start != 'start':
+            raise ValueError('timed out')
+
+class TestBase(unittest.TestCase):
+    '''this test is based on the LocalObjectDatabase and multi_process
+
+the tests execute send, sync, async, ...
+from another process to local references of this process.
+'''
+    @classmethod
+    def setUpClass(cls):
+        cls.pool = Pool(1)
+        cls.pool.apply(thisProcess.call, (g, ()))
+        timeout(lambda:thisProcess.knownProcesses, set())
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.pool.close()
+
+    def setUp(self):
+        self.x = x = X()
+        x.setValue(None)
+        self.ref = ref(x)
+        
+
+class SendTest(TestBase):
+    
+    def test_setValue(self):
+        x = self.x
+        v = self.pool.apply_async(send, (self.ref, 'setValue', \
+                                         (5,), {}))
+        timeout(lambda: x.value, None)
+        self.assertEquals(x.value, 5)
+        self.assertEquals(v.get(), None) # sometimes race-condition
+
+    def test_send_and_error(self):
+        x = self.x
+        stderr, sys.stderr = sys.stderr, StringIO()
+        try:
+            v = self.pool.apply_async(send, (self.ref, 'setValueWithError', \
+                                             ('error',), {}))
+            timeout(lambda: x.value, None)
+        finally:
+            sys.stderr, stderr = stderr, sys.stderr
+        self.assertEquals(x.value, 'error')
+        self.assertNotEquals(stderr.getvalue(), '')
+
+def _call_async(*args):
+    v = async(*args)
+    try:
+        return v.get(TIMEOUT)
+    except:
+        return sys.exc_info()[:2]
+    
+
+class AsyncTest(TestBase):
+
+    def test_setValue(self):
+        v = self.pool.apply(_call_async, (self.ref, 'setValue', ('aValue',), {}))
+        self.assertEquals(v, 'valueSet')
+        self.assertEquals(self.x.value, 'aValue')
+
+##    def test_send_error(self):
+##        ## error - all is dead
+##        try: raise
+##        except: ty, err, tb = sys.exc_info()
+##        self.pool.apply(getattr, (tb, 'tb_next', None))
+
+    def test_setValue_error(self):
+        v = self.pool.apply_async(_call_async, (self.ref, 'setValueWithError', \
+                                          ('aValue',), {}))
+        v = v.get(TIMEOUT)
+        self.assertEquals(v[0], ValueError)
+        self.assertEquals(v[1].args, ('error!',))
+        self.assertEquals(self.x.value, 'aValue')
+
+
+def _sync_call_error(*args):
+    try:
+        sync(*args)
+    except:
+        return sys.exc_info()[:2]
+    raise ValueError('exeption not raised')
+
+class SyncTest(TestBase):
+
+    def test_send_wait(self):
+        x = self.x
+        v = self.pool.apply_async(sync, (self.ref, 'setValue_wait_delete', \
+                                         ('sen_d',), {'after':3}))
+        timeout(lambda:x.value, None)
+        self.assertEquals(x.value, 'sen_d')
+        time.sleep(0.01)
+        self.assertEquals(x.value, 'sen_d')
+        self.assertFalse(v.ready())
+        x.start()
+        timeout(lambda:x.value, 'sen_d')
+        self.assertEquals(x.value, 3)
+        self.assertEquals(v.get(), ('sen_d', 3))
+
+    def test_send_error(self):
+        v = self.pool.apply(_sync_call_error, \
+                                        (self.ref, 'setValueWithError', \
+                                        ('lalala',), {}))
+        self.assertEquals(self.x.value, 'lalala')
+        self.assertEquals(v[0], ValueError)
+        self.assertEquals(v[1].args, ('error!',))
+        
+        
 if __name__ == '__main__':
     unittest.main(exit = False)
